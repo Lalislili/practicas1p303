f00838cd2e48e323990d9f3436594edc553b7ba6
Angelique <angelique@hotmail.com>
2023-09-11 13:14:31 -0500

    Hola Angelique

---
+++ src/practicas/holamundo.java
@@ -0,0 +1,12 @@
+package practicas;
+
+public class holamundo 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hola Mundo Cruel 303");
+        System.out.println();
+	}
+
+}
